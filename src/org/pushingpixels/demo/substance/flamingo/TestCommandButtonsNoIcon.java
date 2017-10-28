17cc31c5057b3067e0258b263b046ade0b445c4d
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2017-10-27 20:22:31 -0400

    Cleanups
    * Tweak the package name
    * Remove unnecessary files
    * Start moving from Tango to Material icons

---
+++ src/org/pushingpixels/demo/substance/flamingo/TestCommandButtonsNoIcon.java
@@ -0,0 +1,30 @@
+package org.pushingpixels.demo.substance.flamingo;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import org.pushingpixels.substance.api.SubstanceLookAndFeel;
+import org.pushingpixels.substance.api.skin.OfficeBlue2007Skin;
+
+public class TestCommandButtonsNoIcon extends
+		org.pushingpixels.demo.flamingo.common.TestCommandButtonsNoIcon {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				SubstanceLookAndFeel.setSkin(new OfficeBlue2007Skin());
+				TestCommandButtonsNoIcon frame = new TestCommandButtonsNoIcon();
+				frame.setSize(800, 400);
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
+				frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
+	}
+
+}
